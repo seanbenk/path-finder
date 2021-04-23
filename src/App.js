2e6879d12b128a839693ce215ba1a9268e0ac42c
seanbenkenstein <seanbenkenstein@gmail.com>
2021-04-23 20:12:35 +1000

    drag and drop start/end points and drawing/deleting walls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import PathFinderGrid from './PathFinderGrid/PathFinderGrid.js'
+import { useContext } from 'react'
+import { PathFinderContext } from './PathFinderContext/PathFinderContext.js'
 
 function App() {
+
+  const { setIsMouseDown } = useContext(PathFinderContext)
+
+  function setMouseUp(){
+    setIsMouseDown(false)
+}
+
   return (
-    <div className="App">
+    <div className="App" onMouseUp={setMouseUp}>
       <PathFinderGrid/>
     </div>
   );
--- src/PathFinderContext/PathFinderContext.js
+++ src/PathFinderContext/PathFinderContext.js
@@ -1,21 +1,82 @@
 import { createContext, useState } from 'react'
+import { flattenDeep, cloneDeep } from 'lodash'
+import dijkstra from '../dijkstra/dijkstra.js'
 
 
 export const PathFinderContext = createContext()
 
 export function PathFinderProvider(props){
 
-    const [nodesGrid, setNodesGrid] = useState(getNewGrid(10,5))
+    const [nodesGrid, setNodesGrid] = useState(getNewGrid(20, 20, {row: 4, col: 3}, {row: 4, col: 6}))
 
-    //Takes a width and height and reset the nodes 
-    const makeNewGrid = (width, height) => {
-        setNodesGrid(getNewGrid(width, height))
+    const [isMouseDown, setIsMouseDown] = useState(false)
+
+    const [selectedNode, setSelectedNode] = useState(null)
+
+    //Takes a width and height and new start/end points, reset the nodes 
+    const makeNewGrid = (width, height, startPointCoord, endPointCoord) => {
+        setNodesGrid(getNewGrid(width, height,startPointCoord,endPointCoord))
+    }
+
+    const selectNode = (row, col) => {
+        setSelectedNode(() => {
+                updateNodes(row, col, nodesGrid[Number(row)][Number(col)])
+                return nodesGrid[Number(row)][Number(col)]
+        })
+    }
+
+    const updateNodes = (row, col, selectedNode) => {
+        const newArr = cloneDeep(nodesGrid)
+        const currNode = newArr[row][col]
+        if(selectedNode.isStartPoint){
+            for(const row of newArr){
+                for(const node of row){
+                    node.isStartPoint = false
+                }
+            }
+            currNode.isStartPoint = true
+            currNode.isEndPoint = false
+            currNode.isWall = false
+        } else if(selectedNode.isEndPoint){
+            for(const row of newArr){
+                for(const node of row){
+                    node.isEndPoint = false
+                }
+            }
+            currNode.isStartPoint = false
+            currNode.isEndPoint = true
+            currNode.isWall = false
+        } else if(selectedNode.isWall && !currNode.isStartPoint && !currNode.isEndPoint){
+            currNode.isStartPoint = false
+            currNode.isEndPoint = false
+            currNode.isWall = false
+        } else if(!currNode.isStartPoint && !currNode.isEndPoint){
+            currNode.isStartPoint = false
+            currNode.isEndPoint = false
+            currNode.isWall = true
+        }
+        setNodesGrid(newArr)
+    }
+
+    const resetGrid = () => {
+        setNodesGrid((prevGrid) => {
+            return prevGrid.map(row => {
+                return row.map( node => {
+                    node.isWall = false
+                    return node
+                })
+            })
+        })
+    }
+
+    const runDijkstra = () => {
+        console.log(dijkstra(nodesGrid))
     }
 
     return (
         <PathFinderContext.Provider 
         value={{
-            nodesGrid, makeNewGrid
+            nodesGrid, setNodesGrid, makeNewGrid, isMouseDown, setIsMouseDown, selectNode, updateNodes, selectedNode, resetGrid, runDijkstra
         }}>
             {props.children}
         </PathFinderContext.Provider>
@@ -48,13 +109,20 @@ class NodeObj{
     }
 }
 
-const getNewGrid = (width, height) => {
+const getNewGrid = (width, height, startCoords, endCoords) => {
     const newGrid = []
     let nodeId = 0
     for(let row = 0; row < height; row++){
         const newRow = []
         for(let col = 0; col < width; col++){
-            newRow.push(new NodeObj(nodeId,row,col,false,false,width,height))
+            if(row === startCoords.row && col === startCoords.col){
+                newRow.push(new NodeObj(nodeId,row,col,true,false,width,height))
+            } else if(row === endCoords.row && col === endCoords.col){
+                newRow.push(new NodeObj(nodeId,row,col,false,true,width,height))
+            } else{
+                newRow.push(new NodeObj(nodeId,row,col,false,false,width,height))
+            }
+
             nodeId++;
         }
         newGrid.push(newRow)
